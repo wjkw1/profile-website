3a094f45c72bc5e8cbde29b8c46e701fad112543
wjkw1 <wilsonw9378@gmail.com>
2023-09-16 12:58:28 +1200

    update the styling of footer for mobile and desktop

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -4,27 +4,25 @@ const padding = 10
 </script>
 
 <template>
-    <footer class="bg-white flex flex-col items-center justify-center py-8 mx-10">
-        <hr class="border-t border-gray-300 mb-6 w-1/2 py-8">
-        <div class="flex justify-between items-center w-full">
-            <div class="flex justify-left">
-                <!-- <a href="#" class="text-gray-500 hover:text-gray-700">&copy; All rights reserved {{ new Date().getFullYear()
-                }}.</a> -->
-                <p class="px-3">&copy; All rights reserved {{ new Date().getFullYear()
-                }}.</p>
-            </div>
-            <div class="flex">
-                <a v-for="social in  appConfig.socials" :href="social.url">
-                    <img :src="social.image" :alt="social.alt" class="h-50 w-50 rounded-full inline-block mx-1">
-                </a>
-            </div>
-            <div class="flex">
+    <footer class="bg-white flex flex-col mx-10 my-8">
+        <hr class="border-t border-gray-300 py-8 w-full">
+        <div class="flex flex-col justify-center sm:flex-row sm:justify-between">
+            <div class="w-full my-1 text-center sm:my-0 sm:text-left">
                 <NuxtLink class="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
                     to="/">Home</NuxtLink>
                 <NuxtLink class="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
                     to="/about">About</NuxtLink>
                 <NuxtLink class="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
                     to="/blog">Blog</NuxtLink>
+            </div>
+            <div class="w-full my-1 text-center sm:my-0">
+                <a v-for="social in  appConfig.socials" :href="social.url">
+                    <img :src="social.image" :alt="social.alt" class="h-50 w-50 rounded-full inline-block mx-1 sm:mx-1">
+                </a>
+            </div>
+            <div class="w-full my-1 text-center sm:my-0 sm:text-right">
+                <p class="px-3">&copy; All rights reserved {{ new Date().getFullYear()
+                }}.</p>
             </div>
         </div>
     </footer>
