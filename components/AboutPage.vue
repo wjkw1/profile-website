d4463b81c9ae1c22a77266f04f30cff63f4222e3
wjkw1 <wilsonw9378@gmail.com>
2023-10-23 10:14:42 +1300

    Update the about page to be more succinct

--- components/AboutPage.vue
+++ components/AboutPage.vue
@@ -2,14 +2,15 @@
     <main class="flex flex-grow mx-10 px-3">
         <div
             class="flex flex-col items-center justify-center text-justify sm:mx-28 md:mx-32 lg:mx-48 xl:mx-48 2xl:xl:mx-48">
-            <NuxtImg src="western-zoom.png" class="rounded-full saturate-100 shadow-xl" height="200" width="200" />
+            <NuxtImg src="western-zoom.png" class="rounded-full saturate-100 shadow-xl my-3 sm:m-10" height="200"
+                width="200" />
             <p class="my-1">Kia ora! I'm a problem solver in the world of IT, based in Wellington. With a BCMS(Hons) degree
                 from the University of Waikato, I've gained experience in Māori mentoring, front-end and back-end
-                development, network engineering, and DevOps. </p>
+                development, network engineering, DevOps and Cloud Engineering. </p>
             <p class="my-1">Currently, I'm contributing to the Papa Reo project at Te Hiku
                 Media, building natural language processing tools for Te Reo Māori.</p>
             <p class="my-1">Beyond IT, I'm an avid app tinkerer,
-                reader, anime fan, fitness enthusiast, and enjoy quality time with friends.</p>
+                reader, anime fan, fitness enthusiast, gamer, and enjoy quality time with friends.</p>
             <p class="my-1">Join me on my journey to make meaningful contributions and solve useful problems in the dynamic
                 landscape of IT.</p>
         </div>
