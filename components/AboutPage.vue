caee00542b1ade9c7fdb602b4f92377e97620099
wjkw1 <wilsonw9378@gmail.com>
2023-10-27 16:06:42 +1300

    Make it so the max width doesn't space to the edge of the page
    Looks nicer on larger screens

--- components/AboutPage.vue
+++ components/AboutPage.vue
@@ -1,17 +1,18 @@
 <template>
-    <main class="flex flex-grow mx-10 px-3">
-        <div
-            class="flex flex-col items-center justify-center text-justify sm:mx-28 md:mx-32 lg:mx-48 xl:mx-48 2xl:xl:mx-48">
+    <main class="flex flex-grow justify-center mx-10 px-3">
+        <div class="flex flex-col items-center justify-center text-justify max-w-2xl">
             <NuxtImg src="western-zoom.png" class="rounded-full saturate-100 shadow-xl my-3 sm:m-10" height="200"
                 width="200" />
-            <p class="my-1">Kia ora! I'm a problem solver in the world of IT, based in Wellington. With a BCMS(Hons) degree
+            <p class="my-1">Kia ora! I'm a problem solver in the world of IT, based in Wellington. With a
+                BCMS(Hons) degree
                 from the University of Waikato, I've gained experience in Māori mentoring, front-end and back-end
                 development, network engineering, DevOps and Cloud Engineering. </p>
-            <p class="my-1">Currently, I'm contributing to the Papa Reo project at Te Hiku
+            <p class="my-1">Currently, I contribute to the Papa Reo project at Te Hiku
                 Media, building natural language processing tools for Te Reo Māori.</p>
             <p class="my-1">Beyond IT, I'm an avid app tinkerer,
                 reader, anime fan, fitness enthusiast, gamer, and enjoy quality time with friends.</p>
-            <p class="my-1">Join me on my journey to make meaningful contributions and solve useful problems in the dynamic
+            <p class="my-1l">Join me on my journey to make meaningful contributions and solve useful problems in
+                the dynamic
                 landscape of IT.</p>
         </div>
     </main>
