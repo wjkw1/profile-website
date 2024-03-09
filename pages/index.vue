6262e51e234c84ee59daf0014f7fe02fb9cc7f2f
wjkw1 <wilsonw9378@gmail.com>
2024-03-09 16:30:58 +1300

    update content and link to relevant stuff
    All the job info is obsolete so I've removed it, you can see that info in my LinkedIn. This space is going to focus on being my place to share things of interest to me.

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,19 @@
 <template>
     <main class="flex grow justify-center mx-10 px-3">
-        <div class="flex flex-col grow items-center justify-center text-center max-w-4xl">
-            <p class="text-2xl md:text-4xl py-10">I'm a Wellington-based problem solver working as a DevOps
-                Engineer at
-                <a href="https://tehiku.nz/" class="text-blue-600 visited:text-purple-600">Te Hiku Media</a>
+        <div class="flex flex-col grow items-center justify-center text-center max-w-5xl">
+
+            <p class="text-2xl py-10">Kia ora and welcome to my slice of the internet!
             </p>
-            <p class="text-2xl md:text-4xl py-10">My latest contribution is on the
-                <a href="https://papareo.nz/" class="text-blue-600 visited:text-purple-600">Papa Reo Project</a>
+            <p class="text-2xl py-10">Check out my <NuxtLink to="/blog" class="text-blue-600 visited:text-purple-600">
+                    blog</NuxtLink>, join me on <a class="text-blue-600 visited:text-purple-600"
+                    href="https://www.goodreads.com/wjkw1">goodreads</a>
+                to see what I'm reading, and
+                take a look at my photos on <a class="text-blue-600 visited:text-purple-600"
+                    href="https://unsplash.com/@wjkw1">Unsplash</a>.
             </p>
-            <p class="text-2xl md:text-4xl py-10">Let's connect on <a href="https://www.linkedin.com/in/western-wilson"
-                    class="text-blue-600 visited:text-purple-600">LinkedIn</a> or
-                <a href="https://github.com/wjkw1" class="text-blue-600 visited:text-purple-600">GitHub</a>
+            <p class="text-2xl py-10">Connect with me on <a class="text-blue-600 visited:text-purple-600"
+                    href="https://www.linkedin.com/in/western-wilson">LinkedIn</a> or any socials above!
             </p>
         </div>
     </main>
-</template>  
+</template>
