134c04dfd72478ed3cffea05248c5c582af2be09
wjkw1 <wilsonw9378@gmail.com>
2024-10-26 21:21:36 +1300

    add credits page to footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -17,15 +17,20 @@ const appConfig = useAppConfig()
                     active-class="rounded-lg px-3 py-2 text-slate-700 font-medium underline hover:bg-slate-100 hover:text-slate-900">
                     Blog
                 </NuxtLink>
+                <NuxtLink to="/credits"
+                    class="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
+                    active-class="rounded-lg px-3 py-2 text-slate-700 font-medium underline hover:bg-slate-100 hover:text-slate-900">
+                    Credits
+                </NuxtLink>
             </div>
             <div class="w-full my-1 text-center items-center space-x-1 sm:my-0">
-                <a v-for="social in  appConfig.socials" :href="social.url">
+                <a v-for="social in appConfig.socials" :href="social.url">
                     <NuxtImg :src="social.image" :alt="social.alt" class="h-8 w-8 inline-block mx-1 sm:mx-1" />
                 </a>
             </div>
             <div class="w-full my-1 text-center sm:my-0 sm:text-right">
                 <p class="px-3">&copy; All rights reserved {{ new Date().getFullYear()
-                }}.</p>
+                    }}.</p>
             </div>
         </div>
     </footer>
