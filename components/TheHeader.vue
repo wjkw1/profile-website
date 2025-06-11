ac95bbdd947c38893dfce67adcefc101b8fd9d72
wjkw1 <wilsonw9378@gmail.com>
2025-06-11 14:37:34 +0700

    Add /now page and update code to use prose instead of relying on class tags.
    * also updated packages running yarn upgrade
    
    This makes it a bit easier to manage going forward as it's simply html tags instead of classes inside <p> tags. It'll probably render better on browser "reader" modes too.

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -16,6 +16,11 @@ const appConfig = useAppConfig()
                     active-class="rounded-lg px-3 py-2 text-slate-700 font-medium underline hover:bg-slate-100 hover:text-slate-900">
                     Home
                 </NuxtLink>
+                <NuxtLink to="/now"
+                    class="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
+                    active-class="rounded-lg px-3 py-2 text-slate-700 font-medium underline hover:bg-slate-100 hover:text-slate-900">
+                    Now
+                </NuxtLink>
                 <NuxtLink to="/blog"
                     class="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
                     active-class="rounded-lg px-3 py-2 text-slate-700 font-medium underline hover:bg-slate-100 hover:text-slate-900">
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,18 @@
 <template>
     <main class="flex grow justify-center mx-10 px-3">
-        <div class="flex flex-col grow items-center justify-center text-center max-w-5xl">
-
-            <p class="text-2xl py-10">Kia ora and welcome to my slice of the internet!
-            </p>
-            <p class="text-2xl py-10">Check out my <NuxtLink to="/blog" class="text-blue-600 visited:text-purple-600">
+        <article class="prose prose-neutral max-w-prose mx-auto">
+            <h1 class="text-2xl font-normal py-9">Kia ora and welcome to my slice of the internet!
+            </h1>
+            <h1 class="text-2xl font-normal py-9">Check out my <NuxtLink to="/blog"
+                    class="text-blue-600 visited:text-purple-600">
                     blog</NuxtLink>, and join me on <a class="text-blue-600 visited:text-purple-600"
                     href="https://www.goodreads.com/wjkw1">goodreads</a>
                 to see what I'm reading.
-            </p>
-            <p class="text-2xl py-10">Connect with me on <a class="text-blue-600 visited:text-purple-600"
+            </h1>
+            <h1 class="text-2xl font-normal py-9">Connect with me on <a class="text-blue-600 visited:text-purple-600"
                     href="https://www.linkedin.com/in/western-wilson">LinkedIn</a> or any socials!
-            </p>
-        </div>
+            </h1>
+        </article>
     </main>
 </template>
 
