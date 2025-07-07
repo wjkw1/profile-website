825691bcf3052cde19c8f945e4742c98de444b7e
wjkw1 <wilsonw9378@gmail.com>
2025-07-07 23:02:21 +0800

    Update site to redirect to my webflow site instead

--- app.config.ts
+++ app.config.ts
@@ -6,30 +6,4 @@ export default defineAppConfig({
       primary: "#ff0000",
     },
   },
-  socials: [
-    {
-      name: "github",
-      alt: "GitHub logo",
-      url: "https://github.com/wjkw1",
-      image: "/logos/icons8-github.svg",
-    },
-    {
-      name: "goodreads",
-      alt: "goodreads logo",
-      url: "https://www.goodreads.com/wjkw1",
-      image: "/logos/icons8-goodreads.svg",
-    },
-    {
-      name: "instagram",
-      alt: "instagram logo",
-      url: "https://www.instagram.com/western.wilson",
-      image: "/logos/icons8-instagram.svg",
-    },
-    {
-      name: "linkedin",
-      alt: "LinkedIn logo",
-      url: "https://www.linkedin.com/in/western-wilson",
-      image: "/logos/icons8-linkedin.svg",
-    },
-  ],
 });
