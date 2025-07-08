5ce1cdd57f62298e2f2c9e5093fd6d8cd023f2fa
wjkw1 <30451128+wjkw1@users.noreply.github.com>
2025-07-08 04:14:07 +0000

    Deploying to gh-pages from @ wjkw1/westernwilson.com@64d35d227f60f6a0c1aad906ad83d753c03a8a61 🚀

--- _nuxt/D8w1MnYI.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./CyMxM1O3.js";import{k as n,o as s,w as f,l as p}from"./BEaIL3hU.js";const u={__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(e){const t=e;return(r,_)=>{const o=a;return s(),n(o,{href:t.href,target:t.target},{default:f(()=>[p(r.$slots,"default")]),_:3},8,["href","target"])}}};export{u as default};
